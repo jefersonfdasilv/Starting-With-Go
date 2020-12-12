816b9fcdb69c6f33fe152847a48067acaadf7127
JEFERSON FERREIRA DA SILVA <jeferson.ferreira@unifesspa.edu.br>
2020-12-12 17:59:53 -0300

    Comprendendo que golang pode ter func com mais de um retorno e o que fazer quando queremos ignorar algum do valores retornados

--- menu.go
+++ menu.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"net/http"
 	"os"
 )
 
@@ -14,6 +15,7 @@ func main() {
 	switch comando {
 	case 1:
 		fmt.Println("Monitorando...")
+		iniciarMonitoramento()
 	case 2:
 		fmt.Println("Exibindo Logs...")
 	case 0:
@@ -45,23 +47,17 @@ func leComando() int {
 
 	return comandoLido
 }
-func exibeIntroducao() {
-	nome := "Douglas"
-	versao := 1.1
-	fmt.Println("Olá, sr.", nome)
-	fmt.Println("Este programa está na versão", versao)
-}
 
-func exibeMenu() {
-	fmt.Println("1- Iniciar Monitoramento")
-	fmt.Println("2- Exibir Logs")
-	fmt.Println("0- Sair do Programa")
-}
+// restante do código omitido
 
-func leComando() int {
-	var comandoLido int
-	fmt.Scan(&comandoLido)
-	fmt.Println("O comando escolhido foi", comandoLido)
+func iniciarMonitoramento() {
+	fmt.Println("Monitorando...")
+	site := "https://www.alura.com.br"
+	resp, _ := http.Get(site)
 
-	return comandoLido
+	if resp.StatusCode == 200 {
+		fmt.Println("Site:", site, "foi carregado com sucesso!")
+	} else {
+		fmt.Println("Site:", site, "está com problemas. Status Code:", resp.StatusCode)
+	}
 }
